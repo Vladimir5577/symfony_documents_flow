2f4bee8f249b0c1915f6f85f49a3ac1fce36ef0a
Vladimir <vladimir160933@gmail.com>
2026-04-28 14:35:33 +0300

    dev

--- src/Enum/UserRole.php
+++ src/Enum/UserRole.php
@@ -7,6 +7,7 @@ namespace App\Enum;
 enum UserRole: string
 {
     case ROLE_ADMIN = 'ROLE_ADMIN';
+    case ROLE_MANAGER_ANALYTIC = 'ROLE_MANAGER_ANALYTIC';
     case ROLE_MANAGER = 'ROLE_MANAGER';
     case ROLE_MODERATOR = 'ROLE_MODERATOR';
     case ROLE_USER = 'ROLE_USER';
@@ -15,6 +16,7 @@ enum UserRole: string
     {
         return match ($this) {
             self::ROLE_ADMIN => 'Администратор',
+            self::ROLE_MANAGER_ANALYTIC => 'Аналитик',
             self::ROLE_MANAGER => 'Управленческая роль',
             self::ROLE_MODERATOR => 'Роль модератор',
             self::ROLE_USER => 'Роль пользователя',
