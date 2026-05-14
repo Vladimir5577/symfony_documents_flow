15861f7dd1694e44cb77597f5cfb76694ab23a31
Vladimir <vladimir160933@gmail.com>
2026-05-14 09:01:45 +0300

    dev

--- src/Enum/UserRole.php
+++ src/Enum/UserRole.php
@@ -10,11 +10,13 @@ enum UserRole: string
     case ROLE_MANAGER_ANALYTIC = 'ROLE_MANAGER_ANALYTIC';
     case ROLE_MANAGER_HR = 'ROLE_MANAGER_HR';
     case ROLE_MANAGER_WORK_CITIZEN_APPEAL = 'ROLE_MANAGER_WORK_CITIZEN_APPEAL';
+    case ROLE_MANAGER_CONTRACT_APPLICATION = 'ROLE_MANAGER_CONTRACT_APPLICATION';
     case ROLE_MANAGER = 'ROLE_MANAGER';
     case ROLE_MODERATOR = 'ROLE_MODERATOR';
     case ROLE_WORK_CITIZEN_APPEAL = 'ROLE_WORK_CITIZEN_APPEAL';
     case ROLE_ANALYTIC = 'ROLE_ANALYTIC';
     case ROLE_HR = 'ROLE_HR';
+    case ROLE_CONTRACT_APPLICATION = 'ROLE_CONTRACT_APPLICATION';
     case ROLE_USER = 'ROLE_USER';
 
     public function getLabel(): string
@@ -24,11 +26,13 @@ enum UserRole: string
             self::ROLE_MANAGER_ANALYTIC => 'Менеджер аналитик',
             self::ROLE_MANAGER_HR => 'Менеджер по кадрам',
             self::ROLE_MANAGER_WORK_CITIZEN_APPEAL => 'Менеджер по работе с обращениями граждан',
+            self::ROLE_MANAGER_CONTRACT_APPLICATION => 'Менеджер по заявкам на договор',
             self::ROLE_MANAGER => 'Менеджер',
             self::ROLE_MODERATOR => 'Роль модератор',
             self::ROLE_WORK_CITIZEN_APPEAL => 'Роль работа с обращениями граждан',
             self::ROLE_ANALYTIC => 'Роль заполнения аналитики',
             self::ROLE_HR => 'Роль кадровика',
+            self::ROLE_CONTRACT_APPLICATION => 'Роль заявок на договор',
             self::ROLE_USER => 'Роль пользователя',
         };
     }
