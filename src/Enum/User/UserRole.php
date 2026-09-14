ebf194552ece84be705c4f1ccc132890ffab213d
Vladimir <vladimir160933@gmail.com>
2026-09-14 11:49:38 +0300

    dev

--- src/Enum/User/UserRole.php
+++ src/Enum/User/UserRole.php
@@ -10,6 +10,7 @@ enum UserRole: string
     case ROLE_ANALYTIC = 'ROLE_ANALYTIC';
     case ROLE_MANAGER = 'ROLE_MANAGER';
     case ROLE_CITIZEN_APPEAL = 'ROLE_CITIZEN_APPEAL';
+    case ROLE_CITIZEN_APPEAL_VOICEMAIL = 'ROLE_CITIZEN_APPEAL_VOICEMAIL';
     case ROLE_HR = 'ROLE_HR';
     case ROLE_CONTRACT_APPLICATION = 'ROLE_CONTRACT_APPLICATION';
     case ROLE_TKO = 'ROLE_TKO';
@@ -30,6 +31,7 @@ enum UserRole: string
             self::ROLE_ANALYTIC => 'Аналитик',
             self::ROLE_MANAGER => 'Менеджер',
             self::ROLE_CITIZEN_APPEAL => 'Роль работа с обращениями граждан',
+            self::ROLE_CITIZEN_APPEAL_VOICEMAIL => 'Роль голосовой почты обращений граждан',
             self::ROLE_HR => 'Роль кадровика',
             self::ROLE_CONTRACT_APPLICATION => 'Роль заявок на договор',
             self::ROLE_TKO => 'Роль аналитики ТКО',
